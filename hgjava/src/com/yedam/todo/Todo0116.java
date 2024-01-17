96112a9f4c33a642160b59e3e490abe24280db41
admin <admin@DESKTOP-HEF96GB>
2024-01-17 09:08:06 +0900

    작업완료

--- hgjava/src/com/yedam/todo/Todo0116.java
+++ hgjava/src/com/yedam/todo/Todo0116.java
@@ -12,13 +12,15 @@ public class Todo0116 {
 			//input.equals("quit")
 		
 			//입력값은 홍길동 입니다.
-			System.out.println(input + " 홍길동 입니다.");
 			//quit 값이 들어오면 console에 <<end of prog>> 출력  
 			if(input.equals("quit")) {
-				System.out.println("end of prog");
+//				System.out.println("end of prog");
 				break;
-				
-			}//if end
+			}
+//			}else{//if end
+			System.out.print("입력값은 " +input + " 입니다.");
+			System.out.println();
+//			}
 		}//while end
 	  }
 }
